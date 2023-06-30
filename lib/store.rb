f727f4ebfb724a353c38200cbeb2f47c66e49718
Mike <mike.simnes@gmail.com>
2023-06-30 10:02:29 -0700

    Add validations and custom validator

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,16 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true,
+  greater_than_or_equal_to: 0 }
+  validates :mens_apparel, :womens_apparel, inclusion: { in: [true, false] }
+  validate :must_carry_mens_or_womens_apparel
+
+
+  def must_carry_mens_or_womens_apparel
+    unless mens_apparel || womens_apparel
+      errors.add(:base, "Store must carry at least one type of apparel")
+    end
+
+  end
 end
