fd646f45c49329f4e3ecf29492a1ff5505698eda
Mike <mike.simnes@gmail.com>
2023-06-30 10:01:31 -0700

    Change hourly_rate

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,8 +11,8 @@ puts "----------"
 # Your code goes here ...
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 @store1.employees.create(first_name: "Vic", last_name: "Itravic", hourly_rate: 52)
-@store2.employees.create(first_name: "Bill", last_name: "Murray", hourly_rate: 34)
-@store2.employees.create(first_name: "Tammy", last_name: "BoBammy", hourly_rate: 17)
+@store2.employees.create(first_name: "Bill", last_name: "Murray", hourly_rate: 44)
+@store2.employees.create(first_name: "Tammy", last_name: "BoBammy", hourly_rate: 117)
 
 # Retrieve all employees from all stores
 # employees = []
