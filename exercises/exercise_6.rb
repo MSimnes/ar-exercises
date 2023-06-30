45c4990846264812b7ca9671ac6bc2843ce5a713
Mike <mike.simnes@gmail.com>
2023-06-30 08:58:15 -0700

    Create new employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,18 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Vic", last_name: "Itravic", hourly_rate: 52)
+@store2.employees.create(first_name: "Bill", last_name: "Murray", hourly_rate: 34)
+@store2.employees.create(first_name: "Tammy", last_name: "BoBammy", hourly_rate: 17)
+
+# Retrieve all employees from all stores
+# employees = []
+# Store.all.each do |store|
+#   employees.concat(store.employees)
+# end
+
+# Print each employee
+# employees.each do |employee|
+#   puts "Name: #{employee.first_name} #{employee.last_name}, Hourly Rate: #{employee.hourly_rate}"
+# end
