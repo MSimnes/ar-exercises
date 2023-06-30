3a8b2c7c4bdbc37f99992b2b422e3b5019d16d99
Mike <mike.simnes@gmail.com>
2023-06-30 08:31:23 -0700

    Get total, average revenue. Get over 1M revenue.

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@Total_revenue_all_stores = Store.all.sum(:annual_revenue)
+puts "Total revenue from all store is $#{@Total_revenue_all_stores}"
+
+@Average_revenue = Store.all.sum(:annual_revenue)/ Store.count
+puts " The average revenue per store is #{@Average_revenue}"
+
+@Stores_over_1M_revenue = Store.all.where(annual_revenue: 1000000..100000000000).count
+
+puts "Number of stores that made over 1 Million in revenue: #{@Stores_over_1M_revenue}"
